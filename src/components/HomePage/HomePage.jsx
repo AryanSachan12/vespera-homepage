cfd3d72fe43fc2389dcf9ca0c023d2ba24c5971f
AryanSachan12 <aryansachan2004@gmail.com>
2024-09-29 23:29:22 +0530

    added-other(pradeep)

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import HeroSection from "./Herosection/heroSection"
 import Events from './Events/Events'
+import Header from './Other/Header'
+import Faq from './Other/Faq'
+import Footer from './Other/Footer'
+import About from './Other/About'
 
 const HomePage = () => {
   return (
     <div>
+        <Header />
         <HeroSection />
+        <About />
         <Events />
+        <Faq />
+        <Footer />
     </div>
   )
 }
